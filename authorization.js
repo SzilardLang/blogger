1d370b543bb4766febc5c6d9abb7fd082c79df69
SzilardLang <148552206+SzilardLang@users.noreply.github.com>
2025-06-21 17:51:36 +0200

    🔒 Inserting Authorization script to DOMContentLoaded

--- authorization.js
+++ authorization.js
@@ -1,9 +1,11 @@
-(function SolAuth(){
-  var blogId = document.body.getAttribute('data-blog-id');
-  if (blogId) {
-    var link = document.createElement('link');
-    link.rel = 'stylesheet';
-    link.href = 'https://www.blogger.com/dyn-css/authorization.css?targetBlogID=' + blogId;
-    document.head.appendChild(link);
-  }
-})();
+document.addEventListener('DOMContentLoaded', function() {
+  (function SolAuth(){
+    var blogId = document.body.getAttribute('data-blog-id');
+    if (blogId) {
+      var link = document.createElement('link');
+      link.rel = 'stylesheet';
+      link.href = 'https://www.blogger.com/dyn-css/authorization.css?targetBlogID=' + blogId;
+      document.head.appendChild(link);
+    }
+  })();
+});
